751733b494a2f1a18da497604211aa65993cf5bc
Lucas day <88118098+LucasAlgerDay@users.noreply.github.com>
2023-08-09 01:26:35 -0600

    purge all files

--- rodadora_chatbot/components/__init__.py
+++
@@ -1,4 +0,0 @@
-from kivy.factory import Factory
-
-Factory.register("PBoxLayout", module="components.box_layout")
-Factory.register("ChatBubble", module="components.chat_bubble")
--- rodadora_chatbot/components/box_layout.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.boxlayout import BoxLayout
-
-from components.adaptive_widget import AdaptiveWidget
-
-
-class PBoxLayout(AdaptiveWidget, BoxLayout):
-    pass
